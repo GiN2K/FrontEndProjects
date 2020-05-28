b73827a36d033430347dea26ef417681da581ac3
Amine AIT-ALI <arabian.blogger.com@gmail.com>
2020-05-28 15:58:15 +0000

    beta3

---
+++ project1/script.js
@@ -0,0 +1,40 @@
+AOS.init();
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+    $('#team-inner').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+  });
